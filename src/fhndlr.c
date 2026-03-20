b3f4fc4f212fe024125469e54a4e4f07369ece95
jesus.loport <jesus.loport@outlook.com>
2026-03-20 06:03:11 -0600

    assignment template

--- src/fhndlr.c
+++ src/fhndlr.c
@@ -3,16 +3,5 @@
 
 void grayscale(unsigned char* pixel)
 {
-    unsigned char blue  = pixel[0];
-    unsigned char green = pixel[1];
-    unsigned char red   = pixel[2];
-    unsigned char gray;
-
-    /* Convert to grayscale */
-    gray = (unsigned char)((red + green + blue) / 3);
-
-    /* Assign grayscale value */
-    pixel[0] = gray;
-    pixel[1] = gray;
-    pixel[2] = gray;
+    void;
 }
