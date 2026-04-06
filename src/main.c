9102c88ec158b02b9f0d6e4b09d0315f7812457b
jesus.loport <jesus.loport@outlook.com>
2026-04-06 15:11:09 -0600

    assignment template

--- src/main.c
+++ src/main.c
@@ -1,44 +1,7 @@
 #include <stdio.h>
 
-/** @brief archivo de salida */
-#define OUTPUT_FILE "../build/out/pyramid.txt"
-
-/** @brief altura de la piramide */
-#define HEIGHT 6
-/** @brief ancho de la piramide */
-#define WIDTH  11
-
 int main()
 {
-    
-    FILE* file = fopen(OUTPUT_FILE, "w");
-    
-    /** 
-     * @brief buffer para capturar linea de texto.
-     * @details ancho (11) + '\0' (1) 
-     */
-    char line[WIDTH + 1];
-
-    /** @details iteramos por filas y columnas */
-    for (int i = 0; i < HEIGHT; i++) {
-        
-        for (int j = 0; j < WIDTH; j++) {
-
-            /** @details llenamos el buffer "line" con lo que corresponda */
-            if (j <= WIDTH / 2 + i &&
-                j >= WIDTH / 2 - i) 
-                line[j] = '*';
-            else 
-                line[j] = ' ';
-
-        }
-
-        /** @details escribimos en el archivo de salida */
-        fprintf(file, "%s\n", line);
-
-    }
-
-    fclose(file);
-
+    printf("Hello World!\n");
     return 0;
 }
